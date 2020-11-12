a651ac6e58f51b35769dae703247a9c7b6764d4e
Nimish Shah <nimishshah2000@gmail.com>
2020-11-12 16:58:37 +0530

    failed changes ...xD

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:isolate';
+import 'dart:ui';
+import 'dart:io';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(
+      debug: true // optional: set false to disable printing logs to console
+      );
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -11,49 +21,46 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   StreamSubscription _intentDataStreamSubscription;
-  List<SharedMediaFile> _sharedFiles;
   String _sharedText;
 
   @override
   void initState() {
     super.initState();
 
-    // For sharing images coming from outside the app while the app is in the memory
-    _intentDataStreamSubscription = ReceiveSharingIntent.getMediaStream()
-        .listen((List<SharedMediaFile> value) {
-      setState(() {
-        _sharedFiles = value;
-        print("Shared:" + (_sharedFiles?.map((f) => f.path)?.join(",") ?? ""));
-      });
-    }, onError: (err) {
-      print("getIntentDataStream error: $err");
-    });
-
-    // For sharing images coming from outside the app while the app is closed
-    ReceiveSharingIntent.getInitialMedia().then((List<SharedMediaFile> value) {
-      setState(() {
-        _sharedFiles = value;
-        print("Shared:" + (_sharedFiles?.map((f) => f.path)?.join(",") ?? ""));
-      });
-    });
-
     // For sharing or opening urls/text coming from outside the app while the app is in the memory
     _intentDataStreamSubscription =
-        ReceiveSharingIntent.getTextStream().listen((String value) {
+        ReceiveSharingIntent.getTextStream().listen((String value) async {
       setState(() {
         _sharedText = value;
         print("Shared: $_sharedText");
       });
+      final taskid = await FlutterDownloader.enqueue(
+              url:
+                  'https://codeload.github.com/sudonims/c-file-manager/zip/master',
+              savedDir: '/sdcard/',
+              showNotification: true)
+          .then((value) => print("Start"));
+      // final tasks = await FlutterDownloader.loadTasks();
+      // print("Download");
+      // awaitf
     }, onError: (err) {
       print("getLinkStream error: $err");
     });
 
     // For sharing or opening urls/text coming from outside the app while the app is closed
-    ReceiveSharingIntent.getInitialText().then((String value) {
+    ReceiveSharingIntent.getInitialText().then((String value) async {
       setState(() {
         _sharedText = value;
         print("Shared: $_sharedText");
       });
+      final taskid = await FlutterDownloader.enqueue(
+              url:
+                  'https://codeload.github.com/sudonims/c-file-manager/zip/master',
+              savedDir: '/sdcard/Download/',
+              showNotification: true)
+          .then((value) => print("object"));
+      // final tasks = await FlutterDownloader.loadTasks();
+      // print("Download");
     });
   }
 
@@ -74,12 +81,6 @@ class _MyAppState extends State<MyApp> {
         body: Center(
           child: Column(
             children: <Widget>[
-              Text("Shared files:", style: textStyleBold),
-              Text(_sharedFiles
-                      ?.map((f) =>
-                          "{Path: ${f.path}, Type: ${f.type.toString().replaceFirst("SharedMediaType.", "")}}\n")
-                      ?.join(",\n") ??
-                  ""),
               SizedBox(height: 100),
               Text("Shared urls/text:", style: textStyleBold),
               Text(_sharedText ?? "")
